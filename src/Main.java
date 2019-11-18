c3ec52812e5685473f26e6d7428e7ede89592a8f
Данил Шмырин <49958853+HenreyMine@users.noreply.github.com>
2019-11-18 11:59:59 +0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import lib.task;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(task.sort("a bbb cccc ddd e"));
+        System.out.println(task.isPhone("+7(123)456-78-90"));
+        System.out.println(task.isPhone("81234567890"));
+    }
+}
