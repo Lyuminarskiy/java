ac5a94ad7ef8b4b1dfb0d5e1f68a883e56eb5a13
IvanovVlad <rr.rr0@mail.ru>
2019-11-25 09:44:13 +0400

    Add files via upload
    Т3.1. Строки и регулярные выражения

---
+++ laba_archevm_1_2/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+import lib.Task3_1;
+
+import java.io.Console;
+import java.util.Random;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println(Task3_1.sort("a bbb cccc ddd e"));
+        System.out.println(Task3_1.isPhone("+7(123)456-78-90"));
+        System.out.println(Task3_1.isPhone("71234567890"));
+    }
+}
