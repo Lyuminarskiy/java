74e783edff55639be6b5d74659c327c1ff070552
Данил Шмырин <49958853+HenreyMine@users.noreply.github.com>
2019-12-02 09:08:43 +0400

    Delete Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import lib.task;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(task.sort("a bbb cccc ddd e"));
-        System.out.println(task.isPhone("+7(123)456-78-90"));
-        System.out.println(task.isPhone("81234567890"));
-    }
-}
