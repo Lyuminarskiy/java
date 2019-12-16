e1e4a9d3fb17f708ee656388a7a4723c42dc37bb
Тихонов Егор Максимович <57887830+TikhonovEM@users.noreply.github.com>
2019-12-16 09:37:32 +0400

    with updated main()

--- Main.java
+++ Main.java
@@ -4,6 +4,20 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        CSVFileEditor.FileJob(args);
+        var FileEditor = new CSVFileEditor(args);
+        switch (FileEditor._command.toLowerCase()){
+            case "show":{
+                FileEditor.show();
+                break;
+            }
+            case "update":{
+                FileEditor.update();
+                break;
+            }
+            case "delete":{
+                FileEditor.delete();
+                break;
+            }
+        }
     }
 }
