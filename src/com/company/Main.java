ef5b291e998cddd4f33a9ea52550cdf698c9a317
Elena Rusaeva <qwexx21@gmail.com>
2019-12-16 11:11:51 +0400

    add Game class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,14 @@
 package com.company;
 
-import lib.*;
-import java.util.ArrayList;
+import lib.Game;
 import java.util.Scanner;
 
 public class Main {
-    public static int goal;
-    public static int currentscore=0;
-    public static ThreadGroup red=new ThreadGroup("Красные");
-    public static ThreadGroup blue=new ThreadGroup("Синие");
     public static void main(String[] args) {
-        ArrayList<Player> players = new ArrayList<>();
-        players.add(new Player(red, "Слава"));
-        players.add(new Player(blue, "Максим"));
-        players.add(new Player(red, "Егор"));
-        players.add(new Player(blue, "Данил"));
-        players.add(new Player(red, "Даша"));
-        players.add(new Player(blue, "Маша"));
-        StringBuilder redTeam= new StringBuilder("Команда 'Красные':");
-        StringBuilder blueTeam= new StringBuilder("Команда 'Синие':");
-        for (Player player:players) {
-            String temp=String.format("\n%s<%s>", player.getName(), player.getThreadGroup().getName());
-            if (player.getThreadGroup().equals(red)) redTeam.append(temp);
-            else blueTeam.append(temp);
-        }
-        System.out.printf("%s\n\n%s\n\n", redTeam.toString(), blueTeam.toString());
         System.out.println("Введите порог победы:");
-        Scanner sk=new Scanner(System.in);
-        goal=sk.nextInt();
-        for (Player player:players) {
-            player.start();
-        }
+        Scanner sk = new Scanner(System.in);
+        int goal = sk.nextInt();
+        Game game = new Game(goal);
+        game.start();
     }
 }
--- src/lib/Player.java
+++ src/lib/Player.java
@@ -1,6 +1,5 @@
 package lib;
 
-import com.company.Main;
 import java.util.Random;
 
 public class Player extends Thread{
@@ -10,27 +9,27 @@ public class Player extends Thread{
     private void printStage(int count){
         System.out.printf("\n\n%s<%s> добавляет %d к своей команде\nТекущий счёт: %d<Красные>, %d<Синие>",
                 Player.currentThread().getName(), Player.currentThread().getThreadGroup().getName(),
-                count, Main.currentscore, -Main.currentscore);
-        if (Math.abs(Main.currentscore) >= Main.goal) {
+                count, Game.currentscore, -Game.currentscore);
+        if (Math.abs(Game.currentscore) >= Game.goal) {
             System.out.printf("\n\nИгра окончена!\nПобедила команда <%s>", Player.currentThread().getThreadGroup().getName());
         }
     }
     public synchronized void run() {
-        while (Math.abs(Main.currentscore) < Main.goal) {
+        while (Math.abs(Game.currentscore) < Game.goal) {
             Random random = new Random();
             try {
                 sleep(100 + random.nextInt(300));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            synchronized ((Object) Main.currentscore) {
-                if (Math.abs(Main.currentscore) < Main.goal) {
-                    int count = random.nextInt(Main.goal);
+            synchronized ((Object) Game.currentscore) {
+                if (Math.abs(Game.currentscore) < Game.goal) {
+                    int count = random.nextInt(Game.goal);
                     if (Player.currentThread().getThreadGroup().getName().equals("Красные")) {
-                        Main.currentscore += count;
+                        Game.currentscore += count;
                     }
                     if (Player.currentThread().getThreadGroup().getName().equals("Синие")) {
-                        Main.currentscore -= count;
+                        Game.currentscore -= count;
                     }
                     this.printStage(count);
                 }
