4e2548f528f251055105633bf3009ce6e66df872
Elena Rusaeva <qwexx21@gmail.com>
2019-12-16 10:03:23 +0400

    multiThread

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,36 @@
 package com.company;
 
-import static lib.Main.*;
+import lib.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+    public static int goal;
+    public static int currentscore=0;
+    public static ThreadGroup red=new ThreadGroup("Красные");
+    public static ThreadGroup blue=new ThreadGroup("Синие");
     public static void main(String[] args) {
-        System.out.println(Sort("ss qqqqqq www eee"));
-        System.out.println(isPhone("+1(123)456-11-22"));
-        System.out.println(isPhone("+1(123)456-11-2a"));
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(new Player(red, "Слава"));
+        players.add(new Player(blue, "Максим"));
+        players.add(new Player(red, "Егор"));
+        players.add(new Player(blue, "Данил"));
+        players.add(new Player(red, "Даша"));
+        players.add(new Player(blue, "Маша"));
+        StringBuilder redTeam= new StringBuilder("Команда 'Красные':");
+        StringBuilder blueTeam= new StringBuilder("Команда 'Синие':");
+        for (Player player:players) {
+            String temp=String.format("\n%s<%s>", player.getName(), player.getThreadGroup().getName());
+            if (player.getThreadGroup().equals(red)) redTeam.append(temp);
+            else blueTeam.append(temp);
+        }
+        System.out.printf("%s\n\n%s\n\n", redTeam.toString(), blueTeam.toString());
+        System.out.println("Введите порог победы:");
+        Scanner sk=new Scanner(System.in);
+        goal=sk.nextInt();
+        for (Player player:players) {
+            player.start();
+        }
     }
 }
