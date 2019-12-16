7eb72f4f40ed9e418e5d3d0b170361aeaacd41df
Elena Rusaeva <qwexx21@gmail.com>
2019-12-16 10:14:51 +0400

    fixes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ import static lib.Main.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Sort("ss qqqqqq www eee"));
+        System.out.println(sort("ss qqqqqq www eee"));
         System.out.println(isPhone("+1(123)456-11-22"));
         System.out.println(isPhone("+1(123)456-11-2a"));
     }
--- src/lib/Main.java
+++ src/lib/Main.java
@@ -6,16 +6,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    public static String Sort(String str){
+    public static String sort(String str){
         String[] splittedStr = str.split(" ");
-        Arrays.sort(splittedStr, (o1, o2) -> -(o1.length() - o2.length()));
+        Arrays.sort(splittedStr, (o1, o2) -> o2.length() - o1.length());
         str = String.join(" ", splittedStr);
         return str;
     }
     public static boolean isPhone(String str){
         Pattern pattern = Pattern.compile("^\\+\\d\\(\\d{3}\\)\\d{3}-\\d{2}-\\d{2}$");
         Matcher matcher = pattern.matcher(str);
-        //            System.out.println(str.substring(matcher.start(), matcher.end()));
         return matcher.find();
     }
 }
