73e3ab37460490b33aea8f991c1f508f7d46cd96
Тихонов Егор Максимович <57887830+TikhonovEM@users.noreply.github.com>
2019-12-16 09:28:49 +0400

    Delete Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import FileEditor.CSVFileEditor;
-
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        var csvFileEditor = new CSVFileEditor(args);
-    }
-}
