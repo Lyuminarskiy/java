c24bed22166c5a9133d3a08183a646c2f059f67f
Vlad <iwanow.wld@gmail.com>
2019-12-20 14:01:26 +0400

    deleted red\blue classes

--- laba_3_4/src/com/company/Main.java
+++ laba_3_4/src/com/company/Main.java
@@ -4,16 +4,8 @@ import lib.*;
 public class Main {
 
     public static void main(String[] args) {
-	    RedTeam rt = new RedTeam();
-	    BlueTeam bt = new BlueTeam();
-        rt.printTeam();
-        bt.printTeam();
-
         Team.setMaxRopeValue(20);
 
         System.out.println("Игра начинается!\nКоманда победит при достижении порога в " + Team.ropeMax + " единиц\n");
-
-        rt.start();
-        bt.start();
     }
 }
