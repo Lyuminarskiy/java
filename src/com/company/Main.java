448cda53bb62a3893495e70a4d5dec16a68b99a6
Данил Шмырин <49958853+HenreyMine@users.noreply.github.com>
2019-12-28 20:53:23 +0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,11 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
 
-    static Integer score,endPoints;
-
     public static void main(String[] args) {
-        score=0;
-        System.out.print("Введите количество очков необходимых для победы: ");
-        var scanner = new Scanner(System.in);
-        endPoints = scanner.nextInt();
+        Game.start();
         ArrayList<Member> redTeam=new ArrayList<Member>(), blueTeam=new ArrayList<Member>();
         redTeam.add(new Member("Иван","Красные"));
         redTeam.add(new Member("Марина","Красные"));
@@ -27,30 +21,12 @@ public class Main {
         for (var member:blueTeam) {
             System.out.printf("%s<%s>\n",member.getName(),member.Color);
         }
-        System.out.printf("\nИгра начинается!\nКоманда победит при достижении порога в %s единиц\n", endPoints);
+        System.out.printf("\nИгра начинается!\nКоманда победит при достижении порога в %s единиц\n", Game.endPoints);
         for (var member:redTeam) {
             member.start();
         }
         for (var member:blueTeam) {
             member.start();
-        }
-    }
-
-    public static synchronized void changeScore(int points, Member member)
-    {
-        if(Math.abs(score)<endPoints)
-        {
-            score += points;
-            System.out.printf("\n%s<%s> добавляет %d к своей команде\nТекущий счет: %d<Красные>,  %d<Синие>\n",
-                    member.getName(), member.Color, Math.abs(points), score, -score);
-            if (Math.abs(score) >= endPoints) {
-                if (score < 0) {
-                    System.out.printf("\nИгра завершена!\nПобедила команда <Синие>!");
-                }
-                if (score > 0) {
-                    System.out.printf("\nИгра завершена!\nПобедила команда <Красные>!");
-                }
-            }
         }
     }
 }
