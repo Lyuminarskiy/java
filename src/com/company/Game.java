af08911159429ec8fb7bd2e5804730d6a0d78584
Данил Шмырин <49958853+HenreyMine@users.noreply.github.com>
2019-12-30 18:29:53 +0400

    Update Game.java

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,16 +1,23 @@
 package com.company;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Game {
 
-    static Integer score;
-    static Integer endPoints;
+    static protected Integer score;
+    static protected Integer endPoints;
 
-    public static void start(){
+    public static void takeEndPoints(){
         score=0;
         System.out.print("Введите количество очков необходимых для победы: ");
         var scanner = new Scanner(System.in);
         endPoints = scanner.nextInt();
+    }
+
+    public static void startMembers(ArrayList<Member> team){
+        for (var member:team) {
+            member.start();
+        }
     }
 
     public static synchronized void changeScore(int points, Member member)
