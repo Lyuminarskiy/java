ace0af69da3c07ad7360c0047103f2f8b3e50f96
Anastasiya-Elesina <58933156+Anastasiya-Elesina@users.noreply.github.com>
2020-01-21 19:08:03 +0400

    Update Player.java

--- src/lib/Player.java
+++ src/lib/Player.java
@@ -4,18 +4,17 @@ import java.util.Random;
 public class Player extends Thread {
 
     String Color;
-    public Player(String name, String color){
+    public Player( String color, String name){
         super(name);
         Color = color;
     }
-
     @Override
     public void run() {
         Random random = new Random();
         while(!Game.isEndGame && Math.abs(Game.score)< Game.endPoints) {
-            if (this.Color == "Синие")
+            if (this.Color == "blue")
                 Game.changeScore(-random.nextInt(100),this);
-            if (this.Color == "Красные")
+            if (this.Color == "red")
                 Game.changeScore(random.nextInt(100),this);
             try {
                 Thread.sleep(random.nextInt(100));
