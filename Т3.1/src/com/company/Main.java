c1c747b155f23dc82e54a25a51cb30d3f288f46d
vovk.kirill <vovk.kirill@mail.ru>
2020-01-26 18:52:02 +0400

    Update Main.java

--- Т3.1/src/com/company/Main.java
+++ Т3.1/src/com/company/Main.java
@@ -1,41 +1,14 @@
 package com.company;
-import java.nio.channels.ScatteringByteChannel;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.*;
-import lib.*;
+
+import lib.lib;
 
 public class Main {
 
+
     public static void main(String[] args) {
-
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input str: ");
-        String str3 = in.nextLine();
-        String[] wordList2 = str3.split(" ");
-
-        libr.sort(wordList2);
-
-        for (int i = 0; i < wordList2.length; i++) {
-            System.out.printf("%s ", wordList2[i]);
-        }
-
-        System.out.print("\n");
-        Scanner in2 = new Scanner(System.in);
-        System.out.print("Input telephone: ");
-        String str4 = in2.nextLine();
-        String[] wordList4 = str4.split("");
-        in2.close();
-        in.close();
-
-        String[] wordList5={"+","7","(",")","-"};
-
-        if (libr.isPhone(wordList4,wordList5)==true) System.out.print("True");
-        else System.out.print("False");
-
+        System.out.println(lib.sort("a bbb cccc ddd e"));
+        System.out.println(lib.sort("1 4444 55555 22 333"));
+        System.out.println(lib.isPhone("+7(123)456-78-90"));
+        System.out.println(lib.isPhone("81234567890"));
     }
-
 }
