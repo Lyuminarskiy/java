c694d2a53eed9e541f468471cf73a557576ba8bc
Алексей <43190360+Krendel338@users.noreply.github.com>
2020-06-04 14:44:01 +0400

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import FileEditor.CSVFileEditor;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        var file = new File(args[0]);
+        var command = args[1];
+        String data;
+        try {
+            data = args[2];
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            data = "all";
+        }
+        var fileReader = new BufferedReader(new FileReader(file));
+        switch (command.toLowerCase()){
+            case "show":{
+                CSVFileEditor.show(data, fileReader);
+                break;
+            }
+            case "update":{
+                CSVFileEditor.update(data, fileReader, file);
+                break;
+            }
+            case "delete":{
+                CSVFileEditor.delete(data, fileReader, file);
+                break;
+            }
+        }
+    }
+}
