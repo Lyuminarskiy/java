000426f2070931f11e61f6d8659dd54f83885310
Алексей <43190360+Krendel338@users.noreply.github.com>
2020-06-04 14:47:28 +0400

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import TugOfWar.*;
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+        final String leftSideTeamName = "Красные";
+        final String rightSideTeamName = "Синие";
+        var leftSideTeam = new ArrayList<TeamMember>();
+        leftSideTeam.add(new TeamMember("Иван", leftSideTeamName));
+        leftSideTeam.add(new TeamMember("Марина", leftSideTeamName));
+        leftSideTeam.add(new TeamMember("Анна", leftSideTeamName));
+        var rightSideTeam = new ArrayList<TeamMember>();
+        rightSideTeam.add(new TeamMember("Михаил", rightSideTeamName));
+        rightSideTeam.add(new TeamMember("Сергей", rightSideTeamName));
+        rightSideTeam.add(new TeamMember("Ольга", rightSideTeamName));
+        Game.start(leftSideTeam, rightSideTeam);
+    }
+}
