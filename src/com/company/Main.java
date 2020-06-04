43875bc8afb30cedd4ea6b2f1d0850123beda3fb
Алексей <43190360+Krendel338@users.noreply.github.com>
2020-06-04 14:35:39 +0400

    Create Main.java

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+import lib.Functions;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Functions.sort("a bbb cccc ddd e"));
+	    System.out.println(Functions.sort("я люблю программирование на Java"));
+	    System.out.println(Functions.isPhone("+7(123)456-78-90"));
+        System.out.println(Functions.isPhone("81234567890"));
+    }
+}
