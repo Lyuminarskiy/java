6cd33c40fc16d09dc1c4b08f09685816e745317e
FuckingNurik <55395710+FuckingNurik@users.noreply.github.com>
2020-10-22 23:27:57 +0400

    Add files via upload

---
+++ laba_archevm_1_2/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+import lib.Task3_1;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println(Task3_1.sort("a bbb cccc ddd e"));
+        System.out.println(Task3_1.isPhone("+7(123)456-78-90"));
+        System.out.println(Task3_1.isPhone("71234567890"));
+    }
+}
