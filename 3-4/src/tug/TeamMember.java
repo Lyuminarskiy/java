d5517611a044c6711ec8be957a4b2600d8206ea7
KirillZdanovsky <64491425+KirillZdanovsky@users.noreply.github.com>
2020-10-24 21:42:12 +0400

    Add files via upload

---
+++ 3-4/src/tug/TeamMember.java
@@ -0,0 +1,26 @@
+package tug;
+
+import java.util.Random;
+
+public class TeamMember extends Thread {
+
+    String Team;
+
+    public TeamMember(String name, String team){
+        super(name);
+        Team = team;
+    }
+
+    @Override
+    public void run() {
+        var r = new Random();
+        while (Game.flag) {
+            Game.pullTheRope(this);
+            try {
+                Thread.sleep(100 + r.nextInt(500));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
